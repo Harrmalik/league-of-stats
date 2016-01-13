b2da7e0f35222a4fe41ddafa6202b4eec149d2f3
Harrmalik <harrmalik@gmail.com>
2016-01-13 09:52:26 -0500

    adding the wiki, bare bones

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-//Initialize Models
-require('./models/Matches.js');
-require('./models/Champions.js');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/league');
+//mongoose.connect('mongodb://localhost:27017/league');
 
-var routes = require('./routes/index');
-var api = require('./routes/api');
+var home = require('./routes/index');
+var wiki = require('./routes/wiki');
 
 var app = express();
 
@@ -25,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/api', api);
+app.use('/', home);
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/wiki.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var key = process.env.API;
+var champions;
+request('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=all&api_key=' + key, function (error, response, body) {
+        body = JSON.parse(body);
+        body = body.data;
+        var body = Object.keys(body).map(function(k) { return body[k] });
+        body.sort(function(a,b){
+         if (a.name < b.name)
+             return -1;
+         if (a.name > b.name)
+             return 1;
+         return 0;
+        });
+        champions = body;
+});
+
+router.get('/', function(req, res){
+   res.render('index');
+});
+
+router.route('/champions')
+   .get(function(req, res){
+      res.render('champions', {'data': champions});
+   });
+
+router.route('/champions/:champID')
+   .get(function(req, res){
+      request('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + req.params.champID + '?champData=all&api_key=' + key, function (error, response, body) {
+         console.log(req.params.champID);
+        body = JSON.parse(body);
+        res.render('champ', {'data': body});
+      });
+   });
+
+router.get('*', function (req, res) {
+  res.send('Page Not Found')
+});
+
+module.exports = router;
